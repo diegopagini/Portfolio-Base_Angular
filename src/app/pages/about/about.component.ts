fa71300a9eabf2cf0c9186545af17279f5dfef25
Diego Pagini <dq@qbkconsulting.com>
2021-02-12 07:57:41 -0300

    firebase conectado

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+  constructor(public infoService: InfoPaginaService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,13 +8,29 @@ import { InfoPage } from '../interfaes/info-page.interface';
 export class InfoPaginaService {
   info: InfoPage;
   cargada: boolean = false;
+  equipo: any[];
 
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     //Leer el archivo JSON
     this.http.get('assets/data/data-page.json').subscribe((resp: InfoPage) => {
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
     });
+  }
+
+  private cargarEquipo() {
+    this.http
+      .get(
+        'https://portfolio-angular-fe6d0-default-rtdb.firebaseio.com/Equipo.json'
+      )
+      .subscribe((resp: any) => {
+        this.equipo = resp;
+        console.log(resp);
+      });
   }
 }
